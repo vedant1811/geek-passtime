5d2f88c933c4b72436ed9fc32b3b8f91563f93e6
vedant <vedant@dunzo.in>
2018-01-13 21:45:22 +0530

    Completed problem TreeDiameter without static variable

--- trees/TreeDiameter.java
+++ trees/TreeDiameter.java
@@ -18,26 +18,17 @@ class Main {
 
     List[] left = diameterArray(root.left);
     List[] right = diameterArray(root.right);
-    System.out.println(Arrays.toString(left));
-    System.out.println(Arrays.toString(right));
-
-    ans[0] = left[0].size() >= right[0].size() ? left[0] : right[0];
-    ans[0].add(root);
-    System.out.println(Arrays.toString(ans));
 
     ans[1] = left[1].size() >= right[1].size() ? left[1] : right[1];
     if (left[0].size() + right[0].size() >= ans[1].size()) {
       ans[1].clear();
-      System.out.println(Arrays.toString(ans));
       ans[1].addAll(left[0]);
-      System.out.println(Arrays.toString(ans));
       ans[1].add(root);
-      System.out.println(Arrays.toString(ans));
       ans[1].addAll(right[0]);
-      System.out.println(Arrays.toString(ans));
     }
-    System.out.println(Arrays.toString(ans));
-    System.out.println("-----");
+
+    ans[0] = left[0].size() >= right[0].size() ? left[0] : right[0];
+    ans[0].add(root);
     return ans;
   }
 
